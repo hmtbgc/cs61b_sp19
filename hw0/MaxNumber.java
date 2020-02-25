20f36a3e49cd2b33fe165292a2f9f51e0895b9d2
hmtbgc <hmtbgc@163.com>
2020-02-25 08:27:24 +0800

    completed hw0

---
+++ hw0/MaxNumber.java
@@ -0,0 +1,16 @@
+public class MaxNumber {
+    public static int max(int[] m) {
+        int maxNumber = m[0];
+        for (int i = 0; i < m.length; i++) {
+            if (maxNumber < m[i]){
+                maxNumber = m[i];
+            }
+        }
+        return maxNumber;
+    }
+
+    public static void main(String[] args) {
+       int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};  
+       System.out.println(max(numbers));    
+    }
+ }
---
+++ hw0/drawTriangle.java
@@ -0,0 +1,14 @@
+public class drawTriangle{
+    public static void DrawTriangle(int N) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        DrawTriangle(10);
+    }
+
+}
