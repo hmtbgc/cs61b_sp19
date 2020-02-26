54d0f6bba1718a6cb899c3d3ad33d11370cfbe39
hmtbgc <hmtbgc@163.com>
2020-02-26 21:52:04 +0800

    completed lab2 first part

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -16,6 +16,12 @@ public class DebugExercise2 {
         return max;
     }
 
+    public static int maxCorrect(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
 
     /** Returns the sum of a and b. Do not step into this function. 
       * This function may have a bug, but if it does, you should find it
@@ -48,7 +54,7 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
+            int biggerValue = maxCorrect(a[i], b[i]);
             returnArray[i] = biggerValue;
         }
 
@@ -60,7 +66,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
